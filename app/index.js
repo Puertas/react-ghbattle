d4e2f9ae7c80355c20354c81cdc04d11afe869bd
Jose Puertas <josepuertas@gmail.com>
2017-01-04 21:13:36 +0100

    Create a Basic Profile view
    3 little controller added inside the main one, to learn about states and props (remember render return should be a Pure function)

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+	name: 'Jose Puertas',
+	username: 'Puertas',
+	image: 'https://avatars0.githubusercontent.com/u/1136445?v=3&u=b4199d5ddf123b740a91e39ba0aed156d4f58b2a&s=400'
+};
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+	render: function() {
+		return <img src ={this.props.imageUrl} style={{height: 100, width: 100}} />;
+	}
+});
+
+var ProfileLink = React.createClass({
 	render: function() {
 		return (
-			<div> Hello World ;) </div>
+			<div>
+				<a href={'https://www.github.com/' + this.props.username}>
+					{this.props.username}
+				</a>
+			</div>
+		);
+	}
+});
+
+var ProfileName = React.createClass({
+	render: function() {
+		return (
+			<div>{this.props.name}</div>
+		);
+	}
+});
+
+var Avatar = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username} />
+			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	<HelloWorld />,
+	<Avatar user={USER_DATA} />,
 	document.getElementById('app')
 );
